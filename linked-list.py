94b630e9505979f78853b56b015451291649d6b4
DaviesHub <davieschuks.a@gmail.com>
2022-10-04 13:06:36 +0200

    Add docstring comment as introduction

---
+++ linked-list.py
@@ -0,0 +1,6 @@
+"""The LinkedList code from before is provided below.
+The three functions added to the LinkedList are:
+"get_position" - returns the element at a certain position.
+"insert" - add an element to a particularspot in the list.
+"delete" - delete the first element with that particular value."""
+
