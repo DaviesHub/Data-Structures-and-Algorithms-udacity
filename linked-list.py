60e713580586581a0b1acfab940243f28a6bd7a8
DaviesHub <davieschuks.a@gmail.com>
2022-10-04 17:12:21 +0200

    Create insert method

--- linked-list.py
+++ linked-list.py
@@ -6,24 +6,24 @@ The three functions added to the LinkedList are:
 "delete" - delete the first element with that particular value."""
 
 class Node():
-    """Model a node of a linked list"""
+    """Model a node of a linked list."""
 
     def __init__(self, value, next=None):
-        """Initialize attributes of a node object"""
+        """Initialize attributes of a node object."""
 
         self.value = value
         self.next = next
 
 class LinkedList():
-    """Model of a linked list"""
+    """Model of a linked list."""
 
     def __init__(self, head=None):
-        """Initialize atttributes of the linked list"""
+        """Initialize atttributes of the linked list."""
 
         self.head = head
 
     def append(self, new_node):
-        """Function to add a node at the end of the linked list"""
+        """Function to add a node at the end of the linked list."""
 
         if self.head:
             current = self.head
@@ -32,3 +32,27 @@ class LinkedList():
             current.next = new_node
         else:
             self.head = new_node
+
+    def get_position(self, position):
+        """Get an node from a particular position. Assume first position is 1."""
+
+        place = 1
+        the_value =  self.value
+        while place < position:
+            current = current.next
+            the_value = current.value
+            place += 1
+        
+        return the_value
+
+    def insert(self, new_node, position):
+        """Inserts a new node at a given position."""
+        
+        place = 1
+        current = self.head
+        while place < position:
+            place += 1
+            current = current.next
+        current = new_node
+            
+
