8906b20362d10aaabf51dc67ea7c31b44f4f1589
DaviesHub <davieschuks.a@gmail.com>
2022-10-04 16:29:39 +0200

    Create list and node classes

--- linked-list.py
+++ linked-list.py
@@ -1,6 +1,26 @@
 """The LinkedList code from before is provided below.
 The three functions added to the LinkedList are:
+"append" - adds an element to the end of the list
 "get_position" - returns the element at a certain position.
 "insert" - add an element to a particularspot in the list.
 "delete" - delete the first element with that particular value."""
 
+class Node():
+    """Model a node of a linked list"""
+
+    def __init__(self, value, next=None):
+        """Initialize attributes of a node object"""
+
+        self.value = value
+        self.next = next
+
+class LinkedList():
+    """Model of a linked list"""
+
+    def __init__(self, head=None):
+        """Initialize atttributes of the linked list"""
+
+        self.head = head
+
+    def append(self, new_node):
+        """Function to add a node at the end of the linked list"""
